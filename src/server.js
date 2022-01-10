3d6cd2de5790097e90e2e49d6a91c06a5b53212a
mikents <mike@nts-apps.com>
2022-01-10 19:55:55 +0000

    updated

--- src/server.js
+++ src/server.js
@@ -1,8 +1,16 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-app.use(express.static(__dirname + '/dist/wc-code-gen'));
-app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+
-'/dist/wc-code-gen/index.html'));});
-app.listen(process.env.PORT || 8080);
+    //Install express server
+    const express = require('express');
+    const path = require('path');
+
+    const app = express();
+
+    // Serve only the static files form the dist directory
+    app.use(express.static(__dirname + '/dist/ngx-starter'));
+
+    app.get('/*', function(req,res) {
+        
+    res.sendFile(path.join(__dirname+'/dist/ngx-starter/index.html'));
+    });
+
+    // Start the app by listening on the default Heroku port
+    app.listen(process.env.PORT || 8080);
