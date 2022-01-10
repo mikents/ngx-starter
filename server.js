e4afc37c1b8ddaa74804d9283101f9971b43da20
mikents <mike@nts-apps.com>
2022-01-10 20:25:25 +0000

    u5

--- server.js
+++ server.js
@@ -5,8 +5,9 @@ const app = express();
 
 app.use(express.static(__dirname + '/dist/ngx-starter'));
 
-app.get('/*', (req,res,next) => {
-    res.sendFile(path.join(__dirname + '/dist/ngx-starter/index.html'));
+app.get('/*', function (req, res) {
+    res.sendFile('index.html', { root: 'dist/ngx-starter/' }
+    );
 });
 
 
