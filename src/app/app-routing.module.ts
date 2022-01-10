0efed7a35a70e3cf7221933b0380635a6dc24589
mikents <mike@nts-apps.com>
2022-01-10 18:41:34 +0000

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: 'code-gen', loadChildren: () => import('./code-gen/code-gen.module').then(m => m.CodeGenModule) }, 
-  { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) }
+  { path: 'code-gen', loadChildren: () => import('./code-gen/code-gen.module').then(m => m.CodeGenModule) }
 ];
 
 @NgModule({
